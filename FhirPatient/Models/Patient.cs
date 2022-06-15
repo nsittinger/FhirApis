4ed90af122bf92806f8b8a2800af9edaea8e19ae
Nick Sittinger <nsittinger@gmail.com>
2022-06-14 20:16:59 -0500

    Initial Commit

---
+++ FhirPatient/Models/Patient.cs
@@ -0,0 +1,3 @@
+﻿namespace FhirPatient.Models;
+
+public record Patient(Guid Id, string FirstName, string LastName);
---
+++ FhirPatient/Program.cs
@@ -0,0 +1,13 @@
+﻿using FhirPatient.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/fhirpatient", () =>
+{
+    var fhirPatient = new Patient(Guid.NewGuid(), "Nick", "Sittinger");
+    return fhirPatient;
+});
+
+app.Run();
